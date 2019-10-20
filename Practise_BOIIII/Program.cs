dcf1aaad571e6ef0860109e32a44728d4d02942f
Sabiha Zaman <sabiha7@outlook.com>
2019-10-20 15:47:15 +0100

    initial commit

---
+++ Practise_BOIIII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practise_BOIIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
